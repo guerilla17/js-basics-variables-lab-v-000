ba0a9c1e6c2c5ed6706b507f933313196d9f57c1
Gigi M <mmgamao1@gmail.com>
2019-04-22 22:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
-const companyName = 'Scuber'
+const companyName = 'Scuber';
 
-let mostProfitableNeighborhood = Chelsea
+let mostProfitableNeighborhood = Chelsea;
 
 let companyCEO = Susan Smith
